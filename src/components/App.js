788005b47d6f2fb4506d4e059beab39bb49f8a8b
Pranav Singhal <crypticzombie@Pranavs-MacBook-Pro.local>
2019-06-05 23:54:24 +0530

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from '../logo.svg';
+import '../App.scss';
+import HomePage from './homePage'
+function App() {
+  return (
+
+          <HomePage />
+
+
+  );
+}
+
+export default App;
---
+++ src/components/navButton.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button'
+
+export default class NavButton extends React.Component{
+    state = {
+        key: 'balue'
+    }
+
+render(){
+
+    return (<>
+        <Button/>
+        <Button/>
+        </>
+    )
+}
+}
+
+
+
+
